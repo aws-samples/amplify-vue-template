432ebf39f8e502d9df47b13680bc2c654f71cb77
Jay Raval <jay.ambrish.raval@gmail.com>
2024-04-18 09:49:33 -0700

    🚀

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import Todos from './components/Todos.vue'
 </script>
 
